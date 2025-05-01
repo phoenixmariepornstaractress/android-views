c6e8447e3288f5cba8b4b889cdf8f0d0192966ce
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-05-01 18:42:09 +0330

    Update MainActivity.java
    package com.zarinpal.views;
    
    import android.os.Bundle;
    import android.support.v7.app.AppCompatActivity;
    import android.util.Log;
    
    import com.zarinpal.libs.views.ZarinTimer;
    
    public class MainActivity extends AppCompatActivity {
    
        private static final String TAG = "MainActivity";
    
        @Override
        protected void onCreate(Bundle savedInstanceState) {
            super.onCreate(savedInstanceState);
            setContentView(R.layout.activity_main);
            setupTimer();
        }
    
        private void setupTimer() {
            ZarinTimer timer = findViewById(R.id.timer);
    
            if (timer != null) {
                timer.startTimer(time -> Log.d(TAG, "Timer running: " + time));
    
                timer.setOnTimeCompleteListener(() -> Log.d(TAG, "Timer finished successfully."));
            } else {
                Log.e(TAG, "Timer view not found. Please check layout.");
            }
        }
    }

--- app/src/main/java/com/zarinpal/views/MainActivity.java
+++ app/src/main/java/com/zarinpal/views/MainActivity.java
@@ -1,37 +1,31 @@
 package com.zarinpal.views;
 
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.zarinpal.libs.views.ZarinTimer;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ZarinTimer timer = findViewById(R.id.timer);
-
-        timer.startTimer(new ZarinTimer.OnTimerListener() {
-            @Override
-            public void onTime(String time) {
-                Log.d(TAG, time);
-            }
-        });
-        timer.setOnTimeCompleteListener(new ZarinTimer.OnTimeCompleteListener() {
-            @Override
-            public void onTimeComplete() {
-                Log.d(TAG, "onTimeComplete: ");
-            }
-        });
-
-
+        setupTimer();
     }
 
+    private void setupTimer() {
+        ZarinTimer timer = findViewById(R.id.timer);
+
+        if (timer != null) {
+            timer.startTimer(time -> Log.d(TAG, "Timer running: " + time));
+
+            timer.setOnTimeCompleteListener(() -> Log.d(TAG, "Timer finished successfully."));
+        } else {
+            Log.e(TAG, "Timer view not found. Please check layout.");
+        }
+    }
 }
