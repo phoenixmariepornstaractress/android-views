ee3a0f4457ce94464597d758bbcc08e2a214b078
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-05-04 21:20:53 +0330

    Update ExampleUnitTest.java

--- app/src/test/java/com/zarinpal/views/ExampleUnitTest.java
+++ app/src/test/java/com/zarinpal/views/ExampleUnitTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void addition_isCorrect() {
+        assertEquals("Expected result of 2 + 2 is 4", 4, 2 + 2);
     }
 }
