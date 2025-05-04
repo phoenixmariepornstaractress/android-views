3a5bdf1d72ea3bf3556151b58aee27e3c77e2c41
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-05-04 22:47:16 +0330

    Update ExampleUnitTest.java

--- app/src/test/java/com/zarinpal/views/ExampleUnitTest.java
+++ app/src/test/java/com/zarinpal/views/ExampleUnitTest.java
@@ -2,16 +2,40 @@ package com.zarinpal.views;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Unit test for basic arithmetic.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * See: http://d.android.com/tools/testing
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals("Expected result of 2 + 2 is 4", 4, 2 + 2);
+    public void testAddition_isCorrect() {
+        int expected = 4;
+        int actual = 2 + 2;
+        assertEquals("Addition result should be 4", expected, actual);
     }
-}
+
+    @Test
+    public void testSubtraction_isCorrect() {
+        int expected = 0;
+        int actual = 2 - 2;
+        assertEquals("Subtraction result should be 0", expected, actual);
+    }
+
+    @Test
+    public void testMultiplication_isCorrect() {
+        int expected = 4;
+        int actual = 2 * 2;
+        assertEquals("Multiplication result should be 4", expected, actual);
+    }
+
+    @Test
+    public void testDivision_isCorrect() {
+        int expected = 1;
+        int actual = 2 / 2;
+        assertEquals("Division result should be 1", expected, actual);
+    }
+} 
